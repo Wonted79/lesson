5db1caae0b6da8ad0c81b7d63b30d878f31572b6
Wonted79 <lgs000423@gmail.com>
2024-06-14 14:46:43 +0900

    ëfirst ninnin

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println("난 닌닌이다!");
         System.out.println("하이");
         Sum sum = new Sum();
         System.out.println(sum.sum());
